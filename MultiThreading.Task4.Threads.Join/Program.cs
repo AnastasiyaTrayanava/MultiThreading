740261d6681a053e435592a6657794a2dd2c7903
AnastasiyaTrayanava <106978099+AnastasiyaTrayanava@users.noreply.github.com>
2023-04-15 23:19:27 +0200

    Add 4 & 5

--- MultiThreading.Task4.Threads.Join/Program.cs
+++ MultiThreading.Task4.Threads.Join/Program.cs
@@ -10,11 +10,15 @@
  */
 
 using System;
+using System.Threading;
 
 namespace MultiThreading.Task4.Threads.Join
 {
     class Program
     {
+        private static int _threadCount = 0;
+        private static int _threadPoolCount = 0;
+        private static SemaphoreSlim _semaphore = new SemaphoreSlim(10);
         static void Main(string[] args)
         {
             Console.WriteLine("4.	Write a program which recursively creates 10 threads.");
@@ -26,9 +30,51 @@ namespace MultiThreading.Task4.Threads.Join
 
             Console.WriteLine();
 
-            // feel free to add your code
+            Console.WriteLine("Thread+Join: ");
+            var thread = new Thread(CreateThreadsRecursively);
+            thread.Start(10);
+            thread.Join();
+
+            Console.WriteLine("ThreadPool+Semaphore: ");
+            ThreadPool.QueueUserWorkItem(CreateThreadPoolRecursively, 10);
 
             Console.ReadLine();
+        }
+
+        private static void CreateThreadPoolRecursively(object state)
+        {
+            if (_threadPoolCount >= 10)
+            {
+                return;
+            }
+
+            _threadPoolCount++;
+
+            var number = (int)state;
+            number--;
+            ThreadPool.QueueUserWorkItem(CreateThreadPoolRecursively, number);
+
+            _semaphore.Wait();
+
+            Console.WriteLine(number);
+        }
+
+        private static void CreateThreadsRecursively(object state)
+        {
+            if (_threadCount >= 10 )
+            {
+                return;
+            }
+
+            var thread = new Thread(CreateThreadsRecursively);
+            _threadCount++;
+
+            var number = (int)state;
+            number--;
+            thread.Start(number);
+            thread.Join();
+
+            Console.WriteLine(number);
         }
     }
 }
--- MultiThreading.Task5.Threads.SharedCollection/Program.cs
+++ MultiThreading.Task5.Threads.SharedCollection/Program.cs
@@ -5,11 +5,18 @@
  * Use Thread, ThreadPool or Task classes for thread creation and any kind of synchronization constructions.
  */
 using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
 
 namespace MultiThreading.Task5.Threads.SharedCollection
 {
     class Program
     {
+        private static object _loqee = new object();
+        private static  List<int> _sharedCollection;
+        private static Mutex _mutex = new Mutex();
+        private static ReaderWriterLockSlim _readerWriterLock = new ReaderWriterLockSlim();
         static void Main(string[] args)
         {
             Console.WriteLine("5. Write a program which creates two threads and a shared collection:");
@@ -18,8 +25,33 @@ namespace MultiThreading.Task5.Threads.SharedCollection
             Console.WriteLine();
 
             // feel free to add your code
+            
+            _sharedCollection = new List<int>();
+
+            for (int i = 0; i < 10; i++)
+            {
+                ThreadPool.QueueUserWorkItem(AddElements, i);
+                ThreadPool.QueueUserWorkItem(PrintCollection);
+            }
 
             Console.ReadLine();
+        }
+
+        private static void PrintCollection(object obj)
+        {
+            lock (_loqee)
+            {
+                Console.WriteLine("Shared collection values: " + string.Join(' ', _sharedCollection));
+
+            }
+        }
+
+        private static void AddElements(object obj)
+        {
+            lock (_loqee)
+            {
+                _sharedCollection.Add((int)obj);
+            }
         }
     }
 }
