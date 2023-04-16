7d1d62c160a88e48ce51a50112d9e54e9162524a
AnastasiyaTrayanava <106978099+AnastasiyaTrayanava@users.noreply.github.com>
2023-04-16 09:34:26 +0200

    Add 6

--- MultiThreading.Task6.Continuation/Program.cs
+++ MultiThreading.Task6.Continuation/Program.cs
@@ -7,6 +7,9 @@
    Demonstrate the work of the each case with console utility.
 */
 using System;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace MultiThreading.Task6.Continuation
 {
@@ -22,9 +25,87 @@ namespace MultiThreading.Task6.Continuation
             Console.WriteLine("Demonstrate the work of the each case with console utility.");
             Console.WriteLine();
 
-            // feel free to add your code
+            CriteriaOne();
+            CriteriaTwo();
+            CriteriaThree();
+            CriteriaFour();
 
             Console.ReadLine();
+        }
+
+        static void CriteriaOne()
+        {
+            Console.WriteLine("Criteria #1:");
+
+            var numberOne = Task.Run(() =>
+            {
+                Console.WriteLine("Executing parent task.");
+            });
+            var numberTwo = numberOne.ContinueWith(x =>
+            {
+                Console.WriteLine("Executing secondary task.");
+            });
+
+            Task.WaitAll();
+        }
+
+        static void CriteriaTwo()
+        {
+            Console.WriteLine("Criteria #2:");
+
+            var numberOne = Task.Run(() =>
+            {
+                Console.WriteLine("Throwing exception...");
+                throw new ArgumentException("Incorrect data");
+            });
+            var numberTwo = numberOne.ContinueWith(x =>
+            {
+                Console.WriteLine("Executing secondary task after parent failure.");
+            }, TaskContinuationOptions.OnlyOnFaulted);
+
+            Task.WaitAll();
+        }
+
+        static void CriteriaThree()
+        {
+            Console.WriteLine("Criteria #3:");
+
+            var numberOne = Task.Run(() =>
+            {
+                Console.WriteLine("Throwing exception...");
+                throw new ArgumentException("Incorrect data");
+            });
+            var numberTwo = numberOne.ContinueWith(x =>
+            {
+                Console.WriteLine("Executing secondary task after parent failure and reusing a thread.");
+            }, TaskContinuationOptions.OnlyOnFaulted & TaskContinuationOptions.ExecuteSynchronously);
+
+            Task.WaitAll();
+        }
+
+        static void CriteriaFour()
+        {
+            var tokenSource = new CancellationTokenSource();
+            var ct = tokenSource.Token;
+            Console.WriteLine("Criteria #4:");
+
+            var numberOne = Task.Run(() =>
+            {
+                Console.WriteLine("Executing parent task.");
+            }, ct);
+
+            tokenSource.Cancel();
+
+            var numberTwo = numberOne.ContinueWith(x =>
+            {
+                if (ct.IsCancellationRequested)
+                {
+                    Console.WriteLine("Task cancelled");
+                }
+                Console.WriteLine("Creating a task outside of thread pool for parent task.");
+            }, TaskContinuationOptions.OnlyOnCanceled);
+
+            Task.WaitAll();
         }
     }
 }
