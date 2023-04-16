e0182d2aa291f0fd9f180f1ff4731d96bcf2c741
AnastasiyaTrayanava <106978099+AnastasiyaTrayanava@users.noreply.github.com>
2023-04-16 12:09:35 +0200

    Fix 4

--- MultiThreading.Task5.Threads.SharedCollection/Program.cs
+++ MultiThreading.Task5.Threads.SharedCollection/Program.cs
@@ -6,17 +6,13 @@
  */
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading;
+using System.Threading.Tasks;
 
 namespace MultiThreading.Task5.Threads.SharedCollection
 {
     class Program
     {
-        private static object _loqee = new object();
-        private static  List<int> _sharedCollection;
-        private static Mutex _mutex = new Mutex();
-        private static ReaderWriterLockSlim _readerWriterLock = new ReaderWriterLockSlim();
+        private static List<int> _sharedCollection;
         static void Main(string[] args)
         {
             Console.WriteLine("5. Write a program which creates two threads and a shared collection:");
@@ -24,14 +20,14 @@ namespace MultiThreading.Task5.Threads.SharedCollection
             Console.WriteLine("Use Thread, ThreadPool or Task classes for thread creation and any kind of synchronization constructions.");
             Console.WriteLine();
 
-            // feel free to add your code
-            
             _sharedCollection = new List<int>();
 
-            for (int i = 0; i < 10; i++)
+            for (var i = 0; i < 10; i++)
             {
-                ThreadPool.QueueUserWorkItem(AddElements, i);
-                ThreadPool.QueueUserWorkItem(PrintCollection);
+                var numberOne = Task.Run(() => AddElements(i));
+                numberOne.Wait();
+                var numberTwo = numberOne.ContinueWith(PrintCollection);
+                numberTwo.Wait();
             }
 
             Console.ReadLine();
@@ -39,19 +35,12 @@ namespace MultiThreading.Task5.Threads.SharedCollection
 
         private static void PrintCollection(object obj)
         {
-            lock (_loqee)
-            {
-                Console.WriteLine("Shared collection values: " + string.Join(' ', _sharedCollection));
-
-            }
+            Console.WriteLine("Shared collection values: " + string.Join(' ', _sharedCollection));
         }
 
         private static void AddElements(object obj)
         {
-            lock (_loqee)
-            {
-                _sharedCollection.Add((int)obj);
-            }
+            _sharedCollection.Add((int)obj);
         }
     }
 }
